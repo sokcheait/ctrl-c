b9214d175c2300db847aa4ed863e49d22f493f55
sokcheait <git config --global user.email>
2022-10-30 21:07:47 +0700

    fix

--- app/Http/Controllers/AdvertisingController.php
+++ app/Http/Controllers/AdvertisingController.php
@@ -113,8 +113,10 @@ class AdvertisingController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Advertising $advertising)
     {
-        //
+        Storage::delete($advertising->image);
+        $advertising->delete();
+        return Redirect::back();
     }
 }
